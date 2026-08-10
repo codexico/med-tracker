6740b2a0a26e98e516c287e07dd44254ae7651bd
Francisco Kahil <codexico@gmail.com>
2026-08-09 22:31:59 -0300

    Release version 4.0.0 (code 14)

--- Kotlin/app/build.gradle.kts
+++ Kotlin/app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.franciscokahil.appMeusRemedinhos"
         minSdk = 24
         targetSdk = 36
-        versionCode = 13
-        versionName = "3.6.0"
+        versionCode = 14
+        versionName = "4.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
