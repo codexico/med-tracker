8018aed776c008687326a13110f12ca8a222e6f2
Francisco Kahil <codexico@gmail.com>
2026-08-15 19:42:54 -0300

    Patch release 4.3.1 (20)

--- Kotlin/app/build.gradle.kts
+++ Kotlin/app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.franciscokahil.appMeusRemedinhos"
         minSdk = 24
         targetSdk = 36
-        versionCode = 19
-        versionName = "4.3.0"
+        versionCode = 20
+        versionName = "4.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
