f2c384cc659cbae374bef1f5768098fe4e38651f
Francisco Kahil <codexico@gmail.com>
2026-08-20 16:59:08 -0300

    Remove RN from main

--- RN/index.ts
+++
@@ -1,6 +0,0 @@
-import 'expo-router/entry-classic';
-import { registerWidgetTaskHandler } from 'react-native-android-widget';
-
-import { widgetTaskHandler } from './widgets/widget-task-handler';
-
-registerWidgetTaskHandler(widgetTaskHandler);
