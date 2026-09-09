f8c4bf35dd7e628fa896b7363005dbf223ec1d6e
Francisco Kahil <codexico@gmail.com>
2026-09-08 23:05:24 -0300

    Release v4.6.0 (26)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.franciscokahil.appMeusRemedinhos"
         minSdk = 24
         targetSdk = 36
-        versionCode = 25
-        versionName = "4.5.0"
+        versionCode = 26
+        versionName = "4.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
